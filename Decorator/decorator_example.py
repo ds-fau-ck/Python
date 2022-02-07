85eb5e0cb11edcfda1ea4026ef86f3726a49b14c
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-02-07 05:18:49 +0100

    update decorate function

--- Decorator/decorator_example.py
+++ Decorator/decorator_example.py
@@ -1,15 +1,26 @@
 
-def decorate_it(func):
-    def adding_extra_behaviour():
-        print("****"*10)
-        func()
-        print("*****"*10)
-    return adding_extra_behaviour
+#def decorate_it(func):
+  #  def adding_extra_behaviour():
+  #      print("****"*10)
+  #      func()
+   #     print("*****"*10)
+    #return adding_extra_behaviour
 
 
-def print_name():
-    print("Hi Kul")
+#def print_name():
+ #   print("Hi Kul")
 
 #print_name()
-my_name=decorate_it(print_name)
-my_name()
+#my_name=decorate_it(print_name)
+#my_name()
+###2nd way of Decorator
+from Decorator import decorate_it
+@decorate_it
+def print_name():
+    print("Hi from kul")
+print_name()
+
+@decorate_it
+def print_welcome():
+    print("Welcome to my Github profile.")
+print_welcome()
